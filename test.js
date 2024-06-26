76d094b5fe04efb240ffd03985d449a47944ae4e
aditya bhojane <adityabhojane66@gmail.com>
2024-06-26 14:44:41 +0530

    add new solution and assignments

--- test.js
+++ test.js
@@ -1,16 +1,23 @@
-function Car(make, model, year) {
-    // Properties
-    this.make = make;
-    this.model = model;
-    this.year = year;
+// function Car(make, model, year) {
+//     // Properties
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
   
-    // Method
-    this.displayInfo = function() {
-      return `${this.year} ${this.make} ${this.model}`;
-    };
-  }
+//     // Method
+//     this.displayInfo = function() {
+//       return `${this.year} ${this.make} ${this.model}`;
+//     };
+//   }
   
-  // Creating an instance of the Car object
-  const myCar =  Car('Toyota', 'Camry', 2022);
+//   // Creating an instance of the Car object
+//   const myCar =  Car('Toyota', 'Camry', 2022);
 
-  Car(1,"j",2)
+//   Car(1,"j",2)
+
+
+let arr = [1,2,3,4,5]
+
+
+remove(4)
+console.log(arr)
